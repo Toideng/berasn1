f6cb7675cde56b8a6a2e824fbd2140d83321ba7f
Toideng <toideng@toideng.com>
2020-05-18 21:47:41 +0000

    exp 154

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
 rem cl -DTARGET_WIN32 berasn1_win.cpp server_win.cpp -Fe"server.exe"
-cl -DTARGET_WIN32 berasn1_win.cpp client_win.cpp -Fe"client.exe"
+rem cl -DTARGET_WIN32 berasn1_win.cpp client_win.cpp -Fe"client.exe"
+cl -DTARGET_WIN32 -c client_win.cpp -Fe"client.obj"
+cl -DTARGET_WIN32 -c berasn1_win.cpp -Fe"berasn1.obj"
 pause
