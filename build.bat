ce6a008807e079c56fca500073ef0a651f4a9cab
Toideng <toideng@toideng.com>
2020-05-18 21:03:53 +0000

    sdf

--- build.bat
+++ build.bat
@@ -1,2 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl -FC -Fo -Zi berasn1_win.cpp
+echo "updated"
+rem call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+rem cl -FC -Fo -Zi berasn1_win.cpp
