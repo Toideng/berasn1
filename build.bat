26b587ef599ef92945019ca3298c3c85a6e78ee1
Toideng <toideng@toideng.com>
2020-05-18 21:11:26 +0000

    Compiler switches

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 echo "updated"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl -FC -Fo -Zi berasn1_win.cpp
+cl -FC -Fo -Zi -DTARGET_WIN32 berasn1_win.cpp
 pause
