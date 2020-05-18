94d3ab24d550541e8f359eda866c00ccc2d3b5bc
Toideng <toideng@toideng.com>
2020-05-18 21:43:19 +0000

    rebuild win

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
-cl -DTARGET_WIN32 berasn1_win.cpp server_win.cpp -Fe server.exe
-cl -DTARGET_WIN32 berasn1_win.cpp client_win.cpp -Fe client.exe
+rem cl -DTARGET_WIN32 berasn1_win.cpp server_win.cpp -Fe"server.exe"
+cl -DTARGET_WIN32 berasn1_win.cpp client_win.cpp -Fe"client.exe"
 pause
